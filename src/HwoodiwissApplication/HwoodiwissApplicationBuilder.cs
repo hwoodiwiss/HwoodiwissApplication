da4dce217d4987bb6bd9f96ceb5a57c3dd2317c9
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2025-12-05 19:31:33 +0000

    Expose some more internals from IHostApplicationBuilder

--- src/HwoodiwissApplication/HwoodiwissApplicationBuilder.cs
+++ src/HwoodiwissApplication/HwoodiwissApplicationBuilder.cs
@@ -1,13 +1,18 @@
 using System.Text.Json.Serialization;
 using Hwoodiwiss.Extensions.Hosting.Extensions;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.Metrics;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
 
 namespace Hwoodiwiss.Extensions.Hosting;
 
-public sealed partial class HwoodiwissApplicationBuilder
+public sealed partial class HwoodiwissApplicationBuilder : IHostApplicationBuilder
 {
     private readonly WebApplicationBuilder _webApplicationBuilder;
     private readonly ApplicationConfiguration _applicationConfiguration = new();
@@ -24,6 +29,19 @@ public sealed partial class HwoodiwissApplicationBuilder
     public IServiceCollection Services => _webApplicationBuilder.Services;
 
     public ConfigurationManager Configuration => _webApplicationBuilder.Configuration;
+
+    public IWebHostEnvironment Environment => _webApplicationBuilder.Environment;
+
+    IConfigurationManager IHostApplicationBuilder.Configuration => Configuration;
+
+    IHostEnvironment IHostApplicationBuilder.Environment => Environment;
+
+    public ILoggingBuilder Logging => _webApplicationBuilder.Logging;
+
+    public IMetricsBuilder Metrics => _webApplicationBuilder.Metrics;
+
+    public IDictionary<object, object> Properties => ((IHostApplicationBuilder)_webApplicationBuilder).Properties;
+
 
     public HwoodiwissApplicationBuilder ConfigureOptions(Action<HwoodiwissApplicationOptions> configureOptions)
     {
@@ -46,6 +64,24 @@ public sealed partial class HwoodiwissApplicationBuilder
         return this;
     }
 
+    public HwoodiwissApplicationBuilder ConfigureMetrics(Action<MeterProviderBuilder> configureMetrics)
+    {
+        ArgumentNullException.ThrowIfNull(configureMetrics);
+
+        _applicationConfiguration.ConfigureMetrics = configureMetrics;
+
+        return this;
+    }
+
+    public HwoodiwissApplicationBuilder ConfigureTracing(Action<TracerProviderBuilder> configureTracing)
+    {
+        ArgumentNullException.ThrowIfNull(configureTracing);
+
+        _applicationConfiguration.ConfigureTracing = configureTracing;
+
+        return this;
+    }
+
     public HwoodiwissApplication Build()
     {
         try
@@ -58,6 +94,11 @@ public sealed partial class HwoodiwissApplicationBuilder
             Log.BuildFailed(_startupLogger, ex);
             throw;
         }
+    }
+
+    public void ConfigureContainer<TContainerBuilder>(IServiceProviderFactory<TContainerBuilder> factory, Action<TContainerBuilder>? configure = null) where TContainerBuilder : notnull
+    {
+        throw new NotImplementedException();
     }
 
     private static partial class Log
