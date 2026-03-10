41b4f1c228c7899ccd324e55607f12c0609e64cb
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2026-03-10 23:54:33 +0000

    Update to .NET 11 Preview 2

--- src/HwoodiwissApplication/Extensions/JsonSerializerOptionExtensions.cs
+++ src/HwoodiwissApplication/Extensions/JsonSerializerOptionExtensions.cs
@@ -5,15 +5,22 @@ namespace HwoodiwissSyncer.Extensions;
 
 public static class JsonSerializerOptionExtensions
 {
+#if NET11_0_OR_GREATER
+    [Obsolete("This method is not needed in .NET 11 and later. Use JsonSerializerOptions.GetTypeInfo<T>() instead.")]
+#endif
     public static JsonTypeInfo<T> GetJsonTypeInfo<T>(this JsonSerializerOptions options)
     {
         ArgumentNullException.ThrowIfNull(options);
 
+#if NET11_0_OR_GREATER
+        return options.GetTypeInfo<T>();
+#else
         if (options.GetTypeInfo(typeof(T)) is JsonTypeInfo<T> jsonTypeInfo)
         {
             return jsonTypeInfo;
         }
 
         throw new ArgumentException($"Unable to find JsonTypeInfo for {typeof(T).FullName}");
+#endif
     }
 }
